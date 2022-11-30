07ed427490b650af14a1bbc5ebdc5ef633531cd9
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-30 20:15:10 +0300

    Update 6.socket_pro.py

--- 6.socket_pro.py
+++ 6.socket_pro.py
@@ -1,33 +1,32 @@
 # An example script to connect to Google using socket
 # programming in Python
+
 import socket # for socket
 import sys
 
-def socket_connect(host):
+def socket_connect(host): # create an INET, STREAMing socket 
     try:
-        global sock
-        sock = socket.socket()
-        print ("Socket successfully created")
-    except socket.error as err:
+        global sock # global variable to use socket outside function
+        sock = socket.socket() # default values are used for arguments
+        print ("Socket successfully created") # socket created
+    except socket.error as err: # if socket creation fails print error
         print ("socket creation failed with error %s" %(err))
 
     # default port for socket
-    port = 80
+    port = 80 # port for socket
 
     try:
-        host_ip = socket.gethostbyname(host)
-    except socket.gaierror:
+        host_ip = socket.gethostbyname(host) # get ip address of host 
+    except socket.gaierror: # if host is not found print error
 
         # this means could not resolve the host
-        print ("there was an error resolving the host")
+        print ("there was an error resolving the host") # error
         sys.exit()
 
     # connecting to the server
-    sock.connect((host_ip, port))
+    sock.connect((host_ip, port)) # connect to host using port 
 
     print ("the socket has successfully connected to google  on port == %s" %(host_ip))
 
 if __name__ == "__main__":
-    socket_connect("www.google.com")
-
-
+    socket_connect('www.google.com')
